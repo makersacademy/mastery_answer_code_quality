924cceb19d44a84c4b841a436e5d4835681e1b1c
Rainer Hind <1928782+rai-hi@users.noreply.github.com>
2021-07-09 10:16:04 +0100

    Add some debugging to try and understand why Hub won't pass mastery quizzes

--- lib/mastery_answer_code_quality/rubocop_execution.rb
+++ lib/mastery_answer_code_quality/rubocop_execution.rb
@@ -4,6 +4,15 @@ class RubocopExecution
   attr_reader :problems
 
   def initialize(rubocop_config_path, answer_location, open_3_class=Open3)
+    puts
+    puts
+    puts "Printing rubocop output for debugging:"
+    puts open_3_class.capture3("rubocop",
+      answer_location.path,
+      "-c",
+      rubocop_config_path)
+    puts
+    puts
     @problems, _, @status =
       open_3_class.capture3("rubocop",
                             answer_location.path,
